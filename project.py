34fdeb4d394d221a4b94cafec34bfbb317850bef
Aayush08102013 <aayush.ranjan0815@gmail.com>
2025-03-02 17:08:10 +1300

    Add files via upload

---
+++ project.py
@@ -0,0 +1,10 @@
+c = "Congratulations" 
+print(c,"in uppercase is:")
+cupp = c.upper()
+print(cupp)
+print(c,"in lowercase is:")
+cloww = c.lower()
+print(cloww)
+print(c, "reversed is:")
+rev_c = c[::-1]
+print(rev_c)
